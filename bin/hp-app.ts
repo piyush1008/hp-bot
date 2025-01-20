47f860ece384829e281dcbc803fe07c9c79ee5c0
Arryan Sinha <arryan.sinha@hp.com>
2025-01-20 13:54:53 +0530

    fix

--- bin/hp-app.ts
+++ bin/hp-app.ts
@@ -20,7 +20,7 @@ function buildStack(){
   switch(pipeline)
   {
      case "hpDev":
-       new HpNlLexLambdasStack(app, 'hp-nl-v2-dev-lex-lambdas',"hpdev", AppConfig.hpDev );
+       new HpNlLexLambdasStack(app, 'hp-nl-v2-dev-lex-lambdas',"hpDev", AppConfig.hpDev );
        break;
 
 
