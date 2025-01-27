6ccc2010089da3c31f7601f0eb0bb7d3412cad88
Arryan Sinha <arryan.sinha@hp.com>
2025-01-27 13:34:52 +0530

    testout

--- src/lambda/LexLambdas/british-english/index.ts
+++ src/lambda/LexLambdas/british-english/index.ts
@@ -1,9 +1,14 @@
+import { I_ENVprops } from "../../../../model/Ienvprops";
 import { BritishRoutingLambda } from "./BritishRoutingLambda";
 
+const _props : I_ENVprops={
+  region: process.env.REGION!,
+  Country_code: process.env.COUNTRY_CODE!,
+  locale: process.env.LOCALE_ID!
+}
 
 export function getLambda(name: string) {
 
-  
     switch (name) {
       case "routing":
         return new BritishRoutingLambda()
@@ -15,5 +20,5 @@ export function getLambda(name: string) {
   
   
   export const routing = async (event: any) => {
-    return await getLambda('routing').handler(event);
+    return await getLambda('routing').validate(event,_props);
   };
