de81406a4a477c2ae65afa469e8a654034caf00b
Arryan Sinha <arryan.sinha@hp.com>
2025-01-28 15:21:17 +0530

    test multi

--- src/lambda/Connect-Lambdas/index.ts
+++ src/lambda/Connect-Lambdas/index.ts
@@ -3,25 +3,13 @@ import { MultiLingualPromptLambda } from "./MultilingualPromptLambda";
 
 
 
-export function getLambda(name: string) {
-
-  
-    switch (name) {
-      case "claimed":
-        return new ClaimedPhNumberLambda()
-      case "multi-prompt":
-        return new MultiLingualPromptLambda()  
-      default:
-        throw new Error(`invalid lambda name ${name}`);
-    }
-  
-  }
-  
-  
+    
   export const ClaimedPhNumber = async (event: any) => {
-    return await getLambda('claimed').handler(event);
+    const ClaimPh = new ClaimedPhNumberLambda()
+    return await ClaimPh.handler(event)
   };
 
   export const MultiLingualPrompt = async (event: any) => {
-    return await getLambda('multi-prompt').handler(event);
+    const MultiPrompt = new MultiLingualPromptLambda()
+    return await MultiPrompt.handler(event)
   };
