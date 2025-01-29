31004f263c66e3116eddf774a25e019219edab47
Arryan Sinha <arryan.sinha@hp.com>
2025-01-29 14:05:08 +0530

    TEST

--- src/lambda/LexLambdas/british-english/BritishRoutingLambda.ts
+++ src/lambda/LexLambdas/british-english/BritishRoutingLambda.ts
@@ -3,12 +3,25 @@ import { RoutingLambda } from "../lib/RoutingLambda";
 import { I_ENVprops } from "../../../../model/Ienvprops";
 
 
-
+const _props : I_ENVprops={
+    region: process.env.REGION!,
+    Country_code: process.env.COUNTRY_CODE!,
+    locale: process.env.LOCALE_ID!
+  }
 
 
 
 
 export class BritishRoutingLambda extends RoutingLambda{
+//     public async handler(event: LexV2Event): Promise<any> {
+           
+//         console.log("routing method",RoutingLambda);
+//         if ((event as any).isWarmer) {
+//             console.log("Warming detected");
+//             return "Lambda is warm." 
+//         }
 
+//         await super.handler(event,_props);  
+// }
 
 }
--- src/lambda/LexLambdas/british-english/index.ts
+++ src/lambda/LexLambdas/british-english/index.ts
@@ -1,6 +1,7 @@
 import { I_ENVprops } from "../../../../model/Ienvprops";
 import { BritishRoutingLambda } from "./BritishRoutingLambda";
 
+
 const _props : I_ENVprops={
   region: process.env.REGION!,
   Country_code: process.env.COUNTRY_CODE!,
@@ -8,18 +9,19 @@ const _props : I_ENVprops={
 }
 
 
-export function getLambda(name: string) {
+// export function getLambda(name: string) {
 
-    switch (name) {
-      case "routing":
-        return new BritishRoutingLambda()
-      default:
-        throw new Error(`invalid lambda name ${name}`);
-    }
+//     switch (name) {
+//       case "routing":
+//         return new BritishRoutingLambda()
+//       default:
+//         throw new Error(`invalid lambda name ${name}`);
+//     }
   
-  }
+// }
   
   
-  export const routing = async (event: any) => {
-    return await getLambda('routing').handler(event,_props);
-  };
+export const routing = async (event: any) => {
+      const britishRout = new BritishRoutingLambda()
+      return await britishRout.handler(event, _props)
+};
